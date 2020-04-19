cb42368e59ac6ff9d070097d5085115d0da379b5
Diego <diego-fidalgo@hotmail.com>
2020-04-19 14:16:45 -0300

    Adicionado exercício 7

--- Point.py
+++ Point.py
@@ -15,3 +15,13 @@ class Point:
     #Exercício 6
     def slope_from_origin (self):
         return self.y/self.x if self.x != 0 else inf
+
+    # Exercício 7
+    def get_line_to(self,other):
+        x0, y0 = self.get()
+        x1, y1 = other.get()
+
+        m = (y1-y0)/(x1-x0)
+        b = y0 - m*x0
+
+        return (m,b)
