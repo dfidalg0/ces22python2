1becc2f7b1f82096b63b3d519c3fb0f4a921a933
Diego <diego-fidalgo@hotmail.com>
2020-04-19 14:08:29 -0300

    Adicionados exercícios 5 e 6

--- Point.py
+++ Point.py
@@ -1,7 +1,17 @@
+from math import inf
+
 class Point:
     def __init__(self, x,y):
         self.x = x
         self.y = y
 
-    def get():
+    def get(self):
         return (self.x, self.y)
+
+    # Exercício 5
+    def reflect_x (self):
+        return Point(self.x,-self.y)
+
+    #Exercício 6
+    def slope_from_origin (self):
+        return self.y/self.x if self.x != 0 else inf
