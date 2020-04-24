860abf0309222c03f7990dfcdbd77366ce3da46c
Diego <diego-fidalgo@hotmail.com>
2020-04-24 16:12:03 -0300

    Adicionado exercício 8

---
+++ ex8.py
@@ -0,0 +1,14 @@
+# Exercício 8
+
+from timeit import default_timer as timer
+
+def time_func (func):
+    t0 = timer()
+    func()
+    print('Execution time:',(timer() - t0) * 1000, 'ms')
+
+@time_func
+def test ():
+    for i in range(1000):
+        print(i+1, end = '\t')
+    print()
