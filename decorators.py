627be027580b82bbcc7a7aefbe0936c265bbba94
Diego <diego-fidalgo@hotmail.com>
2020-04-25 18:35:18 -0300

    Alteração na time_func

--- decorators.py
+++ decorators.py
@@ -5,6 +5,7 @@ from timeit import default_timer as timer
 def time_func (func):
     t0 = timer()
     func()
+    print('Called function', func.__name__)
     print('Execution time:',(timer() - t0) * 1000, 'ms')
 
 @time_func
