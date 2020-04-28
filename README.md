b2210017088695a2692767228e1c3b834b48974a
Diego <diego-fidalgo@hotmail.com>
2020-04-28 11:47:51 -0300

    Adicionado exercício 11

--- README.md
+++ README.md
@@ -9,5 +9,5 @@
 * **5, 6, 7** : point.py
 * **8**       : methods.py
 * **9**       : polygons.py
-* **10, 11**     : decorators.py
+* **10, 11**  : decorators.py
 * **12**      : duck.py, point.py
--- decorators.py
+++ decorators.py
@@ -1,15 +1,27 @@
-# Exercício 8
-
 from timeit import default_timer as timer
 
-def time_func (func):
-    t0 = timer()
-    func()
-    print('Called function', func.__name__)
-    print('Execution time:',(timer() - t0) * 1000, 'ms')
+def time_func (*args,**kwargs):
+    def __time(func):
+        nonlocal args, kwargs
+        t0 = timer()
+        func(*args, **kwargs)
+        elapsed_time = (timer() - t0) * 1000
+        print('Called function', func.__name__)
+        print(f'Execution time: {elapsed_time:.3g} ms')
 
-@time_func
+        return elapsed_time
+
+    return __time
+
+# Exercício 10
+@time_func()
 def test ():
-    for i in range(1000):
+    for i in range(100):
         print(i+1, end = '\t')
     print()
+
+print()
+
+# Exercício 11
+timer1 = time_func ('Testing', 'args', 'list\n', end = 'Testing args dict\n')
+time(print)
